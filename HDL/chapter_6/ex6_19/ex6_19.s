1937335ad62c1f8b70b1f4eb4672cfc8a69e2bb3
roman <kashirov@mail.ru>
2020-05-12 18:36:18 +0700

    упр 6_19

---
+++ HDL/chapter_6/ex6_19/ex6_19.s
@@ -0,0 +1,27 @@
+# Exercise 6.19
+# $a0 = dst array base address
+# $a1 = src array base address
+# $s1 = i
+
+strcpy:
+
+
+
+loop:
+
+
+
+
+
+
+
+
+j loop # repeat
+done:
+jr   $ra            #  return to caller
+
+
+
+
+
+  
