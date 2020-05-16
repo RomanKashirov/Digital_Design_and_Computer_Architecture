9d1e5aec7c20df6d5fde3a732ebc48a3ec9bc6fd
roman <kashirov@mail.ru>
2020-05-16 18:27:39 +0700

    1

--- HDL/chapter_6/ex6_27/ex6_27.s
+++ HDL/chapter_6/ex6_27/ex6_27.s
@@ -1,67 +1,39 @@
-# Exercise 6.27 16.05.2020
-
-
-main:
-	addi $sp, $sp, -4 	 # make room on stack
-	sw   $ra, 0($sp)  	 # store $ra
-	addi $a0, $0, 6
-	
-	jal setArray
-	lw   $ra, 0($sp)  	 # restore $ra
-	addi $sp, $sp, 4	# restore $sp
-	jr $ra
-
-setArray: 
-	addi $sp, $sp, -8 	# make room on stack
-	sw $s0, 4($sp) 		# store $s0
-	sw $ra, 0($sp) 		# store $ra
-	# $t0 = base address of array
-	#lui $t0, 0x1000 		# $t0 = 0x10000000
-	#ori $t0, $t0, 0x7000 	# $t0 = 0x10007000
-	addi $sp, $sp, -40 	# make room for array on stack
-	add $t0, $0, $sp
-	add $s0, $0, $0			# i = 0
-loop:
-	addi $t3, $0, 10
-	slt $t3, $s0, $t3
-	beq $t3, $0, done
-	add $a1, $0, $s0
-	jal compare
-	sw $v0, 0($t0) # array[i] = $v0
-	addi $t0, $t0, 4
-	addi $s0, $s0, 1
-	j loop
-done:
-	nop
-	addi $sp, $sp, 40 # delete array
-	lw $ra, 0($sp) # restore $ra
-	lw $s0, 4($sp) # restore $s0
-	addi $sp, $sp, 8 # restore $sp
-	jr $ra # return
-	
-
-compare: 
-	addi $sp, $sp, -12 	# make room on stack
-	sw $a1, 8($sp)		#store $a1
-	sw $a0, 4($sp) 		# store $a0
-	sw $ra, 0($sp) 		# store $ra
-	jal sub_
-	bgtz $v0, gr
-	beq $v0, $0,gr
-	add $v0, $0, $0
-	j compare_done
-gr:
-	addi $v0, $0, 1
-compare_done:
-	lw $ra, 0($sp) # restore $ra
-	lw $a0, 4($sp) # restore $a0
-	lw $a1, 8($sp) # restore $a1
-	addi $sp, $sp, 12 # restore $sp
-	jr $ra # return	
-	
-sub_:
-	sub $v0, $a0, $a1
-	jr $ra # return
+# Exercise 6.27
+main:		addi $sp, $sp, -4 	# make room on stack
+			sw   $ra, 0($sp)  	# store $ra
+			addi $a0, $0, 3
+			jal setArray
+			lw   $ra, 0($sp)  	# restore $ra
+			addi $sp, $sp, 4	# restore $sp
+			jr $ra
+setArray:	addi $sp, $sp, -48 	# make room on stack
+			sw   $ra,48($sp)   # save return address
+            sw   $s0,44($sp)   # save $s0
+            add $t0, $0, $sp
+			add $s0, $0, $0		# i = 0
+	loop:	addi $t3, $0, 10
+			slt $t3, $s0, $t3
+			beq $t3, $0, done
+			add $a1, $0, $s0
+			jal compare
+			sw $v0, 0($t0) 		# array[i] = $v0
+			addi $t0, $t0, 4
+			addi $s0, $s0, 1
+			j loop
+done:		lw   $s0,44($sp)   # restore $s0
+            lw   $ra,48($sp)   # restore return address
+            addi $sp,$sp,-48    # restore stack pointer
+			jr $ra 				# return
+compare:	addi $sp, $sp, -4 	# make room on stack
+			sw $ra, 0($sp) 		# store $ra
+			jal subtract
+			slt  $v0,$v0,$0     # $v0=1 if sub(a,b) < 0 (return 0)
+			slti $v0,$v0,1      # $v0=1 if sub(a,b)>=0, else $v0 = 0
+			lw $ra, 0($sp) 	    # restore $ra
+			addi $sp, $sp, 4    # restore $sp
+			jr $ra 				# return
+subtract:	sub $v0, $a0, $a1
+			jr $ra 				# return
 
 
 
