c603291790a74bfb336206c570e925d637989cf3
roman <kashirov@mail.ru>
2020-05-21 14:19:15 +0700

    6-35 done

--- HDL/chapter_6/ex6_35/ex6_35.s
+++ HDL/chapter_6/ex6_35/ex6_35.s
@@ -1,10 +1,10 @@
 # Exercise 6.35
 main:		addi $sp, $sp, -4 	# make room on stack
 			sw   $ra, 0($sp)  	# store $ra
-			lui  $a0, 0x451D       # $a0 = 0x451D0000
-			ori  $a0, $a0, 0x1B68  # $a0 = 0x451D1B68 = 2513.713
-			lui  $a1, 0x4049       # $a1 = 0x40490000
-			ori  $a1, $a0, 0x0E56  # $a1 = 40490E56 = 3.1415
+			lui  $a1, 0x4700       # $a1 = 0x47000000
+			ori  $a1, $a1, 0x0000  # $a1 = 0x47000000 = 32768
+			lui  $a0, 0x4049       # $a0 = 0x40490000
+			ori  $a0, $a0, 0x0E56  # $a0 = 0х40490E56 = 3.1415
 			jal add_floats 
 			lw   $ra, 0($sp)  	# restore $ra
 			addi $sp, $sp, 4	# restore $sp
@@ -12,7 +12,45 @@ main:		addi $sp, $sp, -4 	# make room on stack
 add_floats:	addi $sp, $sp, -8 	# make room on stack
 			sw   $s0, 4($sp)  	# store $s0
 			sw   $s1, 0($sp)  	# store $s1
+			add $s0, $0, $a0
+			add $s1, $0, $a1
 			
+			#  Extract exponent and fraction bits
+			srl $t0, $s0, 23	# 1 Exponent
+			srl $t1, $s1, 23	# 2 Exponent
+			
+			sll $t2, $s0, 9
+			srl $t2, $t2, 9		# 1 Fraction
+			
+			sll $t3, $s1, 9
+			srl $t3, $t3, 9		# 2 Fraction
+			
+			# Prepend leading 1 to form the mantissa
+			addi $t4, $0, 1
+			sll $t4, $t4, 23	# $t4 = 0x800000 
+			add $t2, $t2, $t4	# 1 Mantissa
+			add $t3, $t3, $t4 	# 2 Mantissa
+			
+			# Compare exponents, Shift smaller mantissa if necessary
+			sub $t5, $t0, $t1
+			bltz $t5, t1_more
+			srlv $t3, $t3, $t5
+			j done_comp
+t1_more:	sub $t5, $t1, $t0
+			srlv $t2, $t2, $t5
+			add $t0, $0, $t1	# New exponent
+done_comp:	
+			# Add mantissas, Normalize mantissa and adjust exponent if necessary
+			add $t2, $t2, $t3
+			srl $t5, $t2, 24
+			srlv $t2, $t2, $t5
+			sll $t2, $t2, 9
+			srl $t2, $t2, 9		# Fraction 
+			add $t0, $t0, $t5	# Adjusted exponent
+						
+			# Assemble exponent and fraction back into floating-point number
+			sll $t0, $t0, 23	
+			add $v0, $t0, $t2
 			
 			lw   $s1, 0($sp)  	# restore $s1
 			lw   $s0, 4($sp)  	# restore $s0
--- HDL/chapter_6/ex6_35/ex6_35_solution.s
+++ HDL/chapter_6/ex6_35/ex6_35_solution.s
@@ -1,33 +1,56 @@
-# Exercise 6.33
-main:		addi $sp, $sp, -44 	# make room on stack
-			sw   $ra, 40($sp)  	# store $ra
-			add $t0, $0, $sp
-			addi $t1, $0, 0
-			addi $t2, $0, 10
-			addi $t3, $0, 0x1234			
-fill_array:	sw $t3, 0($t0)
-			addi $t0, $t0, 4
-			addi $t1, $t1, 1
-			addi $t3, 0x7999
-			bne $t1, $t2, fill_array
-			add $a0, $0, $sp
-			jal little2big
-			lw   $ra, 40($sp)  	# restore $ra
-			addi $sp, $sp, 44	# restore $sp
+# Exercise 6.35 solution
+# define the masks in the global data segment
+        .data
+mmask:  .word 0x007FFFFF
+emask:  .word 0x7F800000
+ibit:   .word 0x00800000
+obit:   .word 0x01000000
+        .text
+main:		addi $sp, $sp, -4 	# make room on stack
+			sw   $ra, 0($sp)  	# store $ra
+			lui  $a1, 0x4700       # $a1 = 0x47000000
+			ori  $a1, $a1, 0x0000  # $a1 = 0x47000000 = 32768
+			lui  $a0, 0x4049       # $a0 = 0x40490000
+			ori  $a0, $a0, 0x0E56  # $a0 = 0х40490E56 = 3.1415
+			jal add_floats 
+			lw   $ra, 0($sp)  	# restore $ra
+			addi $sp, $sp, 4	# restore $sp
 			jr $ra
-little2big: 
-            addi $t5, $0, 10  # $t5 = i = 10 (loop counter)
-      loop: lb   $t0, 0($a0)  # $t0 = array[i] byte 0
-            lb   $t1, 1($a0)  # $t1 = array[i] byte 1
-            lb   $t2, 2($a0)  # $t2 = array[i] byte 2
-            lb   $t3, 3($a0)  # $t3 = array[i] byte 3
-            sb   $t3, 0($a0)  # array[i] byte 0 = previous byte 3
-            sb   $t2, 1($a0)  # array[i] byte 1 = previous byte 2
-            sb   $t1, 2($a0)  # array[i] byte 2 = previous byte 1
-            sb   $t0, 3($a0)  # array[i] byte 3 = previous byte 0
-            addi $a0, $a0, 4  # increment index into array
-            addi $t5, $t5, -1 # decrement loop counter
-            beq  $t5, $0, done
-            j    loop
-      done:
-			jr $ra
+add_floats:	
+flpadd: 
+		add $s0, $0, $a0
+		add $s1, $0, $a1
+		lw $t4,mmask            # load mantissa mask
+        and $t0,$s0,$t4         # extract mantissa from $s0 (a)
+        and $t1,$s1,$t4         # extract mantissa from $s1 (b)
+        lw $t4,ibit             # load implicit leading 1
+        or $t0,$t0,$t4          # add the implicit leading 1 to mantissa
+        or $t1,$t1,$t4          # add the implicit leading 1 to mantissa
+		lw $t4,emask            # load exponent mask
+        and $t2,$s0,$t4         # extract exponent from $s0 (a)
+        srl $t2,$t2,23          # shift exponent right
+        and $t3,$s1,$t4         # extract exponent from $s1 (b)
+        srl $t3,$t3,23          # shift exponent right
+match:  beq $t2,$t3,addsig      # check whether the exponents match
+        bgeu $t2,$t3,shiftb     # determine which exponent is larger
+shifta: sub $t4,$t3,$t2         # calculate difference in exponents
+        srav $t0,$t0,$t4        # shift a by calculated difference
+        add $t2,$t2,$t4         # update a's exponent
+        j addsig                # skip to the add
+shiftb: sub $t4,$t2,$t3         # calculate difference in exponents
+        srav $t1,$t1,$t4        # shift b by calculated difference
+        add $t3,$t3,$t4         # update b's exponent (not necessary)
+addsig: add $t5,$t0,$t1         # add the mantissas
+norm:   lw $t4,obit             # load mask for bit 24 (overflow bit)
+        and $t4,$t5,$t4         # mask bit 24
+        beq $t4,$0,done         # right shift not needed because bit 24=0
+        srl $t5,$t5,1           # shift right once by 1 bit
+        addi $t2,$t2,1          # increment exponent
+done:   lw $t4,mmask            # load mask
+        and $t5,$t5,$t4         # mask mantissa
+        sll $t2,$t2,23          # shift exponent into place
+        lw $t4,emask            # load mask
+        and $t2,$t2,$t4         # mask exponent
+        or $v0,$t5,$t2          # place mantissa and exponent into $v0
+        jr $ra                  # return to caller
+
