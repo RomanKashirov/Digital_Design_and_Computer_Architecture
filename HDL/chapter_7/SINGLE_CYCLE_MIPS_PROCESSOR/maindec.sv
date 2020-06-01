2f242dd8758297748fc947dc345fd9b6aa05185a
roman <kashirov@mail.ru>
2020-06-01 13:49:19 +0700

    sc mips cpu beginning

--- HDL/chapter_7/SINGLE_CYCLE_MIPS_PROCESSOR/maindec.sv
+++ HDL/chapter_7/SINGLE_CYCLE_MIPS_PROCESSOR/maindec.sv
@@ -15,7 +15,6 @@ module maindec(input  logic [5:0] op,
       6'b000100: controls <= 9'b000100001; // BEQ
       6'b001000: controls <= 9'b101000000; // ADDI
       6'b000010: controls <= 9'b000000100; // J
-      default:   controls <= 9'bxxxxxxxxx; // недействит. 
-инстр.
+      default:   controls <= 9'bxxxxxxxxx; // недействит.инстр.
     endcase
 endmodule
