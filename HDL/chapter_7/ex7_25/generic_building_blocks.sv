645989627074fa4c6bedfe9e5e9bc164e130d53e
roman <kashirov@mail.ru>
2020-06-11 12:21:26 +0700

    упр7-25 1%

--- HDL/chapter_7/ex7_25/generic_building_blocks.sv
+++ HDL/chapter_7/ex7_25/generic_building_blocks.sv
@@ -86,6 +86,15 @@ endmodule
 
 
 
+// 3:1 MULTIPLEXER
+
+// mux3 needed for LUI
+module mux3 #(parameter WIDTH = 8)
+             (input  logic [WIDTH-1:0] d0, d1, d2,
+              input  logic [1:0]       s, 
+              output logic [WIDTH-1:0] y);
+  assign #1 y = s[1] ? d2 : (s[0] ? d1 : d0); 
+endmodule
 
 
 
