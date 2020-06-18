d1224194ac9dee83611744756d5e9af5f9b3688b
roman <kashirov@mail.ru>
2020-06-18 13:38:12 +0700

    7-27 DONE!

--- HDL/chapter_7/ex7_27/testbench.sv
+++ HDL/chapter_7/ex7_27/testbench.sv
@@ -27,7 +27,7 @@ module testbench();
   always @(negedge clk)
    begin
     if (memwrite) begin
-       if (dataadr === 84 & writedata === 224) begin
+       if (dataadr === 84 & writedata === 222) begin
           $display("Simulation succeeded");
           $stop;
       end else if (dataadr !== 80) begin
