c7b664c8296a3f207a2e7ce4eb2bf74f6faeb825
roman <kashirov@mail.ru>
2020-06-22 17:14:07 +0700

    7-40 5%

---
+++ HDL/chapter_7/ex7_40_pipelined_MIPS_processor/datapath.sv
@@ -0,0 +1,95 @@
+// Тракт данных многотактного MIPS-процессора
+
+module datapath(input logic clk, reset, 
+					output logic [31:0] PCF, InstrD, InstrF,
+					input logic RegWriteD, MemtoRegD, MemWriteD, 
+					input logic [2:0] ALUControlD,
+					input logic ALUSrcD, RegDstD, JumpD, BranchD, RegClrD, 
+					output logic EqualD,
+					input logic StallF, StallD, ForwardAD, ForwardBD, 
+					output logic RsD, RtD, 
+					input logic FlushE, 
+					output logic RsE, RtE, 
+					input logic [1:0] ForwardAE, ForwardBE, 
+					output logic [4:0] WriteRegE, 
+					output logic MemtoregE, RegWriteE, 
+					output logic [4:0] WriteRegM,
+					output logic RegWriteM, RegWriteW
+					output logic MemWriteM,
+					output logic [31:0] AluOutM, WriteDataM,
+					input logic [31:0] ReadDataM); 
+					 
+
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+					 
+	
+	// логика адреса памяти
+	flopen #(32)   pcreg(clk, reset, pcen, pcnext, pc);
+	mux2 #(32)    adrmux(pc, aluout, lord, adr);
+								 
+  // логика регистрового файла
+  flopen #(32)   instrreg(clk, reset, irwrite, readdata, instr);
+  flopr #(32)   datareg(clk, reset, rd, data);		//lb
+  mux2 #(5)   wrmux(instr[20:16], instr[15:11], regdst, writereg);
+  mux2 #(32)  wdregmux(aluout, data, memtoreg, writeregdata);
+  signext     se(instr[15:0], signimm);
+  regfile rf(clk, regwrite, instr[25:21], instr[20:16], 
+  writereg, writeregdata, readdataa, readdatab);
+  
+  mux4 #(8)  databytemux(readdata[7:0], readdata[15:8], readdata[23:16], readdata[31:24], adr[1:0], readdatabyte);//lb
+  signext7 se7(readdatabyte, readdata7ex); // lb
+  mux3 #(32)  datamux(readdata, readdata7ex, readdataunex, lb, rd); //lb lbu
+  
+  unsignext7 unse(readdatabyte, readdataunex); // lbu
+  
+   
+  // логика АЛУ
+  flopr #(32)   areg(clk, reset, readdataa, a);
+  flopr #(32)   breg(clk, reset, readdatab, writedata);
+  mux2 #(32)  srcamux(pc, a, alusrca, srca);
+  sl2         immsh(signimm, signimmsh);
+  mux5_32  srcbmux(writedata, 32'b100, signimm, signimmsh, zeroeximm, alusrcb, srcb); // andi
+  alu         alu(srca, srcb, alucontrol, aluresult, zero);
+  zeroext ze(instr[15:0], zeroeximm); // andi
+  
+  
+  // логика следующего pc
+  sl2         addsh(instr, addrsh);
+  flopr #(32)   alureg(clk, reset, aluresult, aluout);
+  mux3 #(32)  pcmux(aluresult, aluout, {pc[31:28], addrsh[27:0]}, pcsrc, pcnext);
+  
+  
+  
+endmodule
--- HDL/chapter_7/ex7_40_pipelined_MIPS_processor/mips.sv
+++ HDL/chapter_7/ex7_40_pipelined_MIPS_processor/mips.sv
@@ -1,45 +1,33 @@
 // Многотактный MIPS-процессор
 
 module mips(input logic clk, reset,
-				output logic [31:0] pc,
-				input logic [31:0] instr,
-				output logic memwrite,
-				output logic [31:0] aluout, writedata,
-				input logic [31:0] readdata);
+				output logic [31:0] PCF,
+				input logic [31:0] InstrF,
+				output logic MemWriteM,
+				output logic [31:0] AluOutM, WriteDataM,
+				input logic [31:0] ReadDataM);
 
 	logic RegWriteD, MemtoRegD, MemWriteD, 
 				ALUSrcD, RegDstD, JumpD, BranchD, RegClrD, EqualD;
-	logic ALUControlD[2:0];
+	logic [2:0]ALUControlD;
+	logic StallF, StallD, ForwardAD, ForwardBD, RsD, RtD, FlushE, RsE, RtE, 
+				  MemtoregE, RegWriteE, 
+				RegWriteM, RegWriteW;
+	logic [1:0] ForwardAE, ForwardBE;
+	logic [4:0] WriteRegE, WriteRegM;
 
-	controller c(instr[31:26], instr[5:0], RegWriteD, MemtoRegD, MemWriteD, ALUControlD[2:0],
+	controller c(InstrD[31:26], InstrD[5:0], RegWriteD, MemtoRegD, MemWriteD, ALUControlD[2:0],
 				ALUSrcD, RegDstD, JumpD, BranchD, RegClrD, EqualD);
 				
-	datapath dp(clk, reset, instr, RegWriteD, MemtoRegD, MemWriteD, ALUControlD[2:0],
-				ALUSrcD, RegDstD, JumpD, BranchD, RegClrD, EqualD 
-				StallF, StallD, ForwardAD, ForwardBD);
-
-				
-				
-				
-				
-				
-				
-				
-				
-//				
-logic memtoreg, alusrc, regdst,
-regwrite, jump, pcsrc, zero;
-
-logic [2:0] alucontrol;
-
-controller c(instr[31:26], instr[5:0], zero,
-				memtoreg, memwrite, pcsrc,
-				alusrc, regdst, regwrite, jump,
-				alucontrol);
-				
-datapath dp(clk, reset, memtoreg, pcsrc,
-				alusrc, regdst, regwrite, jump,
-				alucontrol,
-				zero, pc, instr,
-				aluout, writedata, readdata);
+	datapath dp(clk, reset, PCF, InstrD, InstrF, RegWriteD, MemtoRegD, MemWriteD, ALUControlD,
+				ALUSrcD, RegDstD, JumpD, BranchD, RegClrD, EqualD, 
+				StallF, StallD, ForwardAD, ForwardBD, RsD, RtD, FlushE, RsE, RtE, 
+				ForwardAE, ForwardBE, WriteRegE, MemtoregE, RegWriteE, WriteRegM,
+				RegWriteM, RegWriteW,
+				MemWriteM, AluOutM, WriteDataM, ReadDataM);
+	
+	
+	hazard hu(StallF, StallD, BranchD, JumpD, ForwardAD, ForwardBD, RsD, RtD, FlushE, RsE, RtE, 
+				ForwardAE[1:0], ForwardBE[1:0], WriteRegE[4:0], MemtoregE, RegWriteE, WriteRegM[4:0],
+				RegWriteM, RegWriteW);
 endmodule
